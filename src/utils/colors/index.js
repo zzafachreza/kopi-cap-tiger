886c48a93c7c92b0c472f183f4de46df79f6af1c
Fachreza Maulana <zzafachreza@gmail.com>
2022-08-18 11:45:57 +0700

    Alhamdulillah

---
+++ src/utils/colors/index.js
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: '#744C27',
+  secondary: '#E37423',
+  tertiary: '#000000',
+  background: '#6B63FF',
+  success: '#198754',
+  warning: '#F37120',
+  danger: '#dc3545',
+  black: 'black',
+  white: 'white',
+  border: '#8E8E96',
+  zavalabs: '#E0E0E0',
+  zavalabs2: '#F0EEEE',
+};
